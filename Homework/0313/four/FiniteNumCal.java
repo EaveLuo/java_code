a2dea23992ddc3007497dc4bcd3dc538727b5453
Eave Luo <846899026@qq.com>
2022-03-15 15:29:59 +0800

    fix: the error code

--- Homework/0313/four/FiniteNumCal.java
+++ Homework/0313/four/FiniteNumCal.java
@@ -1,13 +1,15 @@
 public class FiniteNumCal {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; sum <= 9876; i++) {
-            int n = 1;
-            for (int j = 1; j <= i; j++) {
-                n *= j;
+        int n = 1;
+        while (sum <= 9876) {
+            n++;
+            int i = 1;
+            for (int j = 1; j <= n; j++) {
+                i *= j;
             }
-            sum += n;
+            sum += i;
         }
-        System.out.println("res=" + sum);
+        System.out.println("res=" + (n - 1));
     }
 }
